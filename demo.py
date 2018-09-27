3247e70afc1eab3e8602de049d98b3666d10a569
Joseph Madich <joseph.madich@LOH070W374ZHTD6.cardinalhealth.net>
2018-09-27 10:39:11 -0400

    basic demo pt. 2 adding css

--- demo.py
+++ demo.py
@@ -5,6 +5,14 @@ import dash_html_components as html
 
 app = dash.Dash()
 
+# Dash CSS
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+# Bootstrap CSS
+# app.css.append_css({"external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"})
+# app.css.append_css({"external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"})
+# Loading screen CSS
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/brPBPO.css"})
+
 app.layout = html.H1('On Dasher!')
 
 if __name__ == '__main__':
