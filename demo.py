4de4c32ead714de9bcc009d9659409f00ff3823c
Joseph Madich <joseph.madich@LOH070W374ZHTD6.cardinalhealth.net>
2018-09-27 11:09:35 -0400

    adding simple visualization

--- demo.py
+++ demo.py
@@ -18,7 +18,20 @@ app.layout = html.Div(children=[
 
     html.Div(children='''
         Dash: A web application framework for Python.
-    ''')
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
 ])
 
 if __name__ == '__main__':
